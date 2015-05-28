e1fc922d96f5530cc59d739207cf1e0aecda9572
Ron Kurr <kurr@kurron.org>
2015-05-28 12:35:01 -0400

    Support the new instance

--- connect-aws-linux.sh
+++ connect-aws-linux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ssh -v -i ~/aws/aws-linux.pem ubuntu@ec2-52-24-117-186.us-west-2.compute.amazonaws.com
+ssh -v -i ~/aws/aws-linux.pem ec2-user@ec2-52-26-36-26.us-west-2.compute.amazonaws.com
